05fb75bb6feff96d535d4ec41e4a1b90295f7981
Ramsy de Vos <ramsy@ramsy.it>
2020-05-30 14:14:16 +0200

    Update run script

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
+kubectl create namespace ingress-nginx
 helm install --namespace ingress-nginx ingress-nginx ingress-nginx/ingress-nginx
+
+kubectl --namespace ingress-nginx patch svc ingress-nginx-controller -p '{"spec":{"externalTrafficPolicy":"Local"}}'
 
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
 helm repo add jetstack https://charts.jetstack.io
